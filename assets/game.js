9eac66b62bbe061b9979523557a3700e4ba92a82
skinsler <simonkinsler@gmail.com>
2019-04-29 17:32:23 -0500

    initial commit

---
+++ assets/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    function startGame() {
+
+        for (let i=0; i<4; i++) {
+            var crystal = $("<div>").val("crystal" + i);
+        }
+    };
+
+    startGame();
+
+});
