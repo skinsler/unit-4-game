22973f0f692aa04ca5c87a58f685ee681dd3335d
skinsler <simonkinsler@gmail.com>
2019-04-29 21:44:02 -0500

    added ids to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,34 @@
+const NUMBER_CRYSTALS = 4;
+const MAX_CRYSTAL_VALUE = 20;
+const MIN_GOAL_VALUE = 30;
+const MAX_GOAL_VALUE = 150;
+
+var wins = 0;
+var losses = 0;
+var goal;
+var score;
+var crystalValues = []
+
+
 $(document).ready(function() {
+
+ 
+
+
+
 
     function startGame() {
 
-        for (let i=0; i<4; i++) {
-            var crystal = $("<div>").val("crystal" + i);
-        }
+
     };
 
     startGame();
+
+    $(".crystal").on("click", function() {
+        let crystalId = $(this).attr('id');
+        console.log(crystalId);
+
+        if 
+        });
 
 });
